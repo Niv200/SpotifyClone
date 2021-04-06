8e63be5a1413b817320a8cf6f9e23207f1f73fb4
Nivm200 <Nivm200@gmail.com>
2021-04-07 01:44:03 +0300

    Opened first routes

--- musicapp/src/App.js
+++ musicapp/src/App.js
@@ -1,20 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import Song from './components/Song';
+import logo from "./logo.svg";
+import "./App.css";
+import Song from "./components/Song";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Song 
-      name = "song name"
-      artist = "song artist"
-      album = "song album"
-      length = "3:00"
-      link = "https://www.youtube.com/embed/YmynMyn8o6E"
-      lyrics = "lyrics lyrics lyrics bla bla bla"
-      />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/playlists">Playlists</Link>
+            </li>
+            <li>
+              <Link to="/artists">Artists</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+            <li>
+              <Link to="/songs">Songs</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/playlists">
+              <h1>Playlists</h1>
+            </Route>
+            <Route path="/artists">
+              <h1>Artists</h1>
+            </Route>
+            <Route path="/albums">
+              <h1>Albums</h1>
+            </Route>
+            <Route path="/songs">
+              <h1>Songs</h1>
+            </Route>
+            <Route path="/">
+              <h1>Home</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <Song 
+name = "song name"
+artist = "song artist"
+album = "song album"
+length = "3:00"
+link = "https://www.youtube.com/embed/YmynMyn8o6E"
+lyrics = "lyrics lyrics lyrics bla bla bla"
+/> */
+}
